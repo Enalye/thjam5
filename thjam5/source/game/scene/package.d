4d6887cc46806f3c8ea18bf03a91cf4524ee0c53
Enalye <enalyen@gmail.com>
2020-01-17 20:32:50 +0100

    Basic wall functions & example

--- thjam5/source/game/scene/package.d
+++ thjam5/source/game/scene/package.d
@@ -2,5 +2,9 @@ module game.scene;
 
 public {
     import game.scene.world;
+    import game.scene.actor;
+    import game.scene.solid;
+    import game.scene.player;
+    import game.scene.wall;
 }
 
---
+++ thjam5/source/game/script/wall.d
@@ -0,0 +1,76 @@
+module game.script.wall;
+
+import atelier, grimoire;
+import game.scene;
+
+package void loadWall(GrData data) {
+    auto defWall = data.addUserType("Wall");
+
+    data.addPrimitive(&_createWall, "createWall", ["x", "y", "hx", "hy"], [grInt, grInt, grInt, grInt], [defWall]);
+    
+    data.addPrimitive(&_setWallPosition, "setPosition", ["wall", "x", "y"], [defWall, grInt, grInt]);
+    data.addPrimitive(&_getWallPosition, "getPosition", ["wall"], [defWall], [grInt, grInt]);
+    data.addPrimitive(&_getWallPositionX, "getPositionX", ["wall"], [defWall], [grInt]);
+    data.addPrimitive(&_getWallPositionY, "getPositionY", ["wall"], [defWall], [grInt]);
+
+    data.addPrimitive(&_setWallHitbox, "setHitbox", ["wall", "x", "y"], [defWall, grInt, grInt]);
+    data.addPrimitive(&_getWallHitbox, "getHitbox", ["wall"], [defWall], [grInt, grInt]);
+    data.addPrimitive(&_getWallHitboxX, "getHitboxX", ["wall"], [defWall], [grInt]);
+    data.addPrimitive(&_getWallHitboxY, "getHitboxY", ["wall"], [defWall], [grInt]);
+
+    data.addPrimitive(&_moveWall, "move", ["wall", "x", "y"], [defWall, grFloat, grFloat]);
+
+}
+
+private void _createWall(GrCall call) {
+    auto wall = new Wall(Vec2i(call.getInt("x"), call.getInt("y")), Vec2i(call.getInt("hx"), call.getInt("hy")));
+    spawnSolid(wall);
+    call.setUserData!Wall(wall);
+}
+
+private void _setWallPosition(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    wall.position = Vec2i(call.getInt("x"), call.getInt("y"));
+}
+
+private void _getWallPosition(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    call.setInt(wall.position.x);
+    call.setInt(wall.position.y);
+}
+
+private void _getWallPositionX(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    call.setInt(wall.position.x);
+}
+
+private void _getWallPositionY(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    call.setInt(wall.position.y);
+}
+
+private void _setWallHitbox(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    wall.hitbox = Vec2i(call.getInt("x"), call.getInt("y"));
+}
+
+private void _getWallHitbox(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    call.setInt(wall.hitbox.x);
+    call.setInt(wall.hitbox.y);
+}
+
+private void _getWallHitboxX(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    call.setInt(wall.hitbox.x);
+}
+
+private void _getWallHitboxY(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    call.setInt(wall.hitbox.y);
+}
+
+private void _moveWall(GrCall call) {
+    Wall wall = call.getUserData!Wall("wall");
+    wall.move(call.getFloat("x"), call.getFloat("y"));
+}
