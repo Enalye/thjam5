0efbe6bfd67ac41e07b92f38a2a686ee0b3eb564
Jonath Kane <jonathkane@gmail.com>
2020-01-19 20:32:41 +0100

    bug fix please fix camera clamp

--- thjam5/source/game/scene/background.d
+++ thjam5/source/game/scene/background.d
@@ -8,6 +8,8 @@ class Background {
     Sprite _boss1;
     Sprite _boss2;
 
+    this() {}
+
     void load(string backgroundName, string boss1Name, string boss2Name) {
         _background = fetch!Sprite(backgroundName);
         _boss1      = fetch!Sprite(boss1Name);
@@ -17,10 +19,10 @@ class Background {
     }
 
     void draw() {
-        /*if(_background !is null) {
+        if(_background !is null) {
             _background.draw(Vec2f.zero);
             _boss1.draw(Vec2f(0f, 250f));
             _boss2.draw(Vec2f(400f, 400f));
-        }*/
+        }
     }
 }
